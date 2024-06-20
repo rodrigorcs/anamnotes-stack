ebd2049a16e93e5170b8b7cfb46bc2f06b74e02c
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2024-06-20 11:19:35 -0300

    Removed reponse data

--- src/handlers/start-instance.ts
+++ src/handlers/start-instance.ts
@@ -1,14 +1,11 @@
 import 'reflect-metadata'
 import { logger } from '../common/powertools/logger'
-import { APIGatewayProxyEvent, Context } from 'aws-lambda'
+// import { APIGatewayProxyEvent, Context } from 'aws-lambda'
 import { successResponse, errorResponse } from '../lib/helpers/responses'
 
-export const handler = (event: APIGatewayProxyEvent, context: Context) => {
+export const handler = () => {
   try {
-    return successResponse({
-      event,
-      context,
-    })
+    return successResponse()
   } catch (error) {
     logger.error(JSON.stringify(error))
     return errorResponse(400, error as Error)
