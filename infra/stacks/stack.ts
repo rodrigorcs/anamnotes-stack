599eea1dd88e684dc1bec4ef79197456d5acb80a
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2024-06-21 18:21:02 -0300

    Added back lambda

--- infra/stacks/stack.ts
+++ infra/stacks/stack.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import { config } from '../config'
@@ -18,19 +17,19 @@ export class AnamnotesStack extends Stack {
 
     // LAMBDAS
 
-    // const { functionMap: infraLambdas } = new GroupedLambdaFunctions(this, {
-    //   type: ELambdaGroupTypes.INFRA,
-    //   sharedEnvs: sharedLambdaEnvs,
-    //   functionProps: {
-    //     startInstance: {
-    //       reservedConcurrentExecutions: 1,
-    //       memoryMB: 256,
-    //       timeoutSecs: 300,
-    //       sourceCodePath: '../dist/handlers/start-instance',
-    //       environment: {},
-    //     },
-    //   },
-    // })
+    const { functionMap: infraLambdas } = new GroupedLambdaFunctions(this, {
+      type: ELambdaGroupTypes.INFRA,
+      sharedEnvs: sharedLambdaEnvs,
+      functionProps: {
+        startInstance: {
+          reservedConcurrentExecutions: 1,
+          memoryMB: 256,
+          timeoutSecs: 300,
+          sourceCodePath: '../dist/handlers/start-instance',
+          environment: {},
+        },
+      },
+    })
 
     // API GATEWAY
 
@@ -40,23 +39,23 @@ export class AnamnotesStack extends Stack {
 
     const baseResourceV1 = api.root.addResource('v1')
 
-    // new NestedApiResources(this, {
-    //   baseResource: baseResourceV1,
-    //   routes: [
-    //     {
-    //       resourcePath: ['prepare'],
-    //       integrations: [
-    //         {
-    //           method: HttpMethods.POST,
-    //           handler: infraLambdas.startInstance,
-    //           apigwMethodOptions: {
-    //             operationName: 'Start instance',
-    //             apiKeyRequired: false,
-    //           },
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // })
+    new NestedApiResources(this, {
+      baseResource: baseResourceV1,
+      routes: [
+        {
+          resourcePath: ['prepare'],
+          integrations: [
+            {
+              method: HttpMethods.POST,
+              handler: infraLambdas.startInstance,
+              apigwMethodOptions: {
+                operationName: 'Start instance',
+                apiKeyRequired: false,
+              },
+            },
+          ],
+        },
+      ],
+    })
   }
 }
