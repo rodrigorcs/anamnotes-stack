b88872a61fd2a6de0d93e84be9916763d28a5345
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2024-06-23 13:31:55 -0300

    Added ASG security group

--- infra/lib/constructs/ec2/asg.ts
+++ infra/lib/constructs/ec2/asg.ts
@@ -17,14 +17,24 @@ export class AutoScalingGroup {
   public readonly group: autoscaling.AutoScalingGroup
 
   constructor(scope: Construct, props: IAutoScalingGroupProps) {
-    const groupName = `${config.projectName}-${props.name}-group`
+    const asgName = `${config.projectName}-${props.name}-group`
+
+    // SECURITY GROUPS
+    const securityGroupName = `${config.projectName}-${props.name}-sg`
+    const securityGroup = new ec2.SecurityGroup(scope, securityGroupName, {
+      vpc: props.vpc,
+      securityGroupName,
+      allowAllOutbound: true,
+    })
+    securityGroup.connections.allowFromAnyIpv4(ec2.Port.allTraffic())
+    securityGroup.connections.allowToAnyIpv4(ec2.Port.allTraffic())
 
     const keyPair = props.keyPairName
-      ? ec2.KeyPair.fromKeyPairName(scope, `${groupName}-key-pair`, props.keyPairName)
+      ? ec2.KeyPair.fromKeyPairName(scope, `${asgName}-key-pair`, props.keyPairName)
       : undefined
 
-    this.group = new autoscaling.AutoScalingGroup(scope, groupName, {
-      autoScalingGroupName: groupName,
+    this.group = new autoscaling.AutoScalingGroup(scope, asgName, {
+      autoScalingGroupName: asgName,
       vpc: props.vpc,
       instanceType: new ec2.InstanceType(props.instanceType),
       maxCapacity: props.maxCapacity,
@@ -32,6 +42,7 @@ export class AutoScalingGroup {
       machineImage: props.machineImage,
       allowAllOutbound: true,
       keyPair,
+      securityGroup,
     })
 
     if (props.commandsOnBoot) this.group.addUserData(...props.commandsOnBoot)
