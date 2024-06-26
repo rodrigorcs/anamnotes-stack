e9ed9d43d0d34069101b0c4947c3de1e7500d50a
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2024-06-26 11:54:23 -0300

    Added logs

--- src/handlers/connect.ts
+++ src/handlers/connect.ts
@@ -1,6 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { APIGatewayProxyEvent } from 'aws-lambda'
+import { logger } from '../common/powertools/logger'
 
 export const handler = async (event: APIGatewayProxyEvent) => {
+  logger.info('Received event', { event })
   return { statusCode: 200, body: 'Connected.' }
 }
