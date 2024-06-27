887c9bd6752458a7d569bce0d52302319f3c754b
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2024-06-27 18:50:13 -0300

    Updated stageValue

--- infra/bin/index.ts
+++ infra/bin/index.ts
@@ -3,13 +3,13 @@ import 'source-map-support/register'
 import { App } from 'aws-cdk-lib'
 import { AnamnotesStack } from '../stacks/stack'
 import { config } from '../config'
-import { stageValue } from '../lib/utils'
+import { stageValue } from '../lib/helpers'
 
 export const createStack = (app: App) =>
   new AnamnotesStack(app, config.projectName, {
     env: config.stack.env,
     description: `Resources for ${config.stage} Anamnotes`,
-    terminationProtection: stageValue.bool(
+    terminationProtection: stageValue(
       {
         production: true,
       },
