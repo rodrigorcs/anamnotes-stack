ff7f0d8f9ce981d49c2ae3ae78fbc39dd3daaa89
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2024-06-29 20:07:54 -0300

    Added conversationId to websocket connect

--- src/handlers/connect-ws.ts
+++ src/handlers/connect-ws.ts
@@ -1,19 +1,28 @@
-import { APIGatewayProxyWebsocketEventV2, APIGatewayProxyWebsocketHandlerV2 } from 'aws-lambda'
+import { APIGatewayProxyWebsocketEventV2 as DefaultEvent, Handler } from 'aws-lambda'
 import { logger } from '../common/powertools/logger'
 import { WebSocketConnectionsRepository } from '../repositories/WebSocketConnectionsRepository'
 import dayjs from 'dayjs'
 import { middyWrapper } from '../common/middy'
 
+interface APIGatewayProxyWebsocketEventV2 extends DefaultEvent {
+  queryStringParameters?: Record<string, string>
+  multiValueQueryStringParameters?: Record<string, string[]>
+}
+type APIGatewayProxyWebsocketHandlerV2 = Handler<APIGatewayProxyWebsocketEventV2, void>
+
 export const handler: APIGatewayProxyWebsocketHandlerV2 =
   middyWrapper<APIGatewayProxyWebsocketEventV2>(async (event) => {
     logger.info('Received event', { event })
-    console.log('Received event', { event })
+
+    const conversationId = event.queryStringParameters?.conversationId
+    if (!conversationId) throw new Error('conversationId is missing in query string parameters')
+
     const wsConnectionsRepository = new WebSocketConnectionsRepository()
 
     const createdConnection = await wsConnectionsRepository.create({
       id: event.requestContext.connectionId,
       userId: 'test-userId',
-      conversationId: 'test-conversationId',
+      conversationId,
       createdAt: dayjs(),
     })
 
