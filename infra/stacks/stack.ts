2c4a0de91a14d693f063c9f1db473344161c7284
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2024-06-29 17:35:51 -0300

    Added back constructs

--- infra/stacks/stack.ts
+++ infra/stacks/stack.ts
@@ -1,9 +1,9 @@
 import {
   Stack,
   StackProps,
-  // aws_apigateway as apigw,
-  // aws_certificatemanager as certificateManager,
-  // aws_route53 as route53,
+  aws_apigateway as apigw,
+  aws_certificatemanager as certificateManager,
+  aws_route53 as route53,
 } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import { config } from '../config'
@@ -279,34 +279,34 @@ export class AnamnotesStack extends Stack {
       },
     })
 
-    // const customDomain = new apigw.DomainName(this, 'customDomain', {
-    //   domainName: 'api.anamnotes.com',
-    //   certificate: certificateManager.Certificate.fromCertificateArn(
-    //     this,
-    //     'ACM_Certificate',
-    //     'arn:aws:acm:us-east-1:735967209496:certificate/4da2fd45-4693-483e-84d3-d506823b9b48',
-    //   ),
-    //   endpointType: apigw.EndpointType.EDGE,
-    // })
+    const customDomain = new apigw.DomainName(this, 'customDomain', {
+      domainName: 'api.anamnotes.com',
+      certificate: certificateManager.Certificate.fromCertificateArn(
+        this,
+        'ACM_Certificate',
+        'arn:aws:acm:us-east-1:735967209496:certificate/4da2fd45-4693-483e-84d3-d506823b9b48',
+      ),
+      endpointType: apigw.EndpointType.EDGE,
+    })
 
-    // // Associate the Custom domain that we created with new APIGateway using BasePathMapping:
-    // new apigw.BasePathMapping(this, 'CustomBasePathMapping', {
-    //   domainName: customDomain,
-    //   restApi: restApi,
-    // })
+    // Associate the Custom domain that we created with new APIGateway using BasePathMapping:
+    new apigw.BasePathMapping(this, 'CustomBasePathMapping', {
+      domainName: customDomain,
+      restApi: restApi,
+    })
 
-    // // Get a reference to AN EXISTING hosted zone using the HOSTED_ZONE_ID. You can get this from route53
-    // const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
-    //   hostedZoneId: 'Z0448513YG2VMTOLT2NK',
-    //   zoneName: 'anamnotes.com',
-    // })
+    // Get a reference to AN EXISTING hosted zone using the HOSTED_ZONE_ID. You can get this from route53
+    const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
+      hostedZoneId: 'Z0448513YG2VMTOLT2NK',
+      zoneName: 'anamnotes.com',
+    })
 
-    // // Finally, add a CName record in the hosted zone with a value of the new custom domain that was created above:
-    // new route53.CnameRecord(this, 'ApiGatewayRecordSet', {
-    //   zone: hostedZone,
-    //   recordName: 'api',
-    //   domainName: customDomain.domainNameAliasDomainName,
-    // })
+    // Finally, add a CName record in the hosted zone with a value of the new custom domain that was created above:
+    new route53.CnameRecord(this, 'ApiGatewayRecordSet', {
+      zone: hostedZone,
+      recordName: 'api',
+      domainName: customDomain.domainNameAliasDomainName,
+    })
 
     // PERMISSIONS
 
