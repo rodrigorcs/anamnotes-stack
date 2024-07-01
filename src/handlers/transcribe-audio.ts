b34419b7a3beb9629c8b60b8fcdce0f9d5345a4f
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2024-07-01 13:16:02 -0300

    Updated provider to dummy

--- src/handlers/transcribe-audio.ts
+++ src/handlers/transcribe-audio.ts
@@ -45,7 +45,7 @@ const getRecordPromises = (event: SQSEvent) => {
           }
 
           try {
-            const AIProvider = AIProviderSwitcher.getProvider(AIProviders.OPEN_AI)
+            const AIProvider = AIProviderSwitcher.getProvider(AIProviders.DUMMY)
             const contentSections = await AIProvider.transcribe({
               fileByteArray,
               fileName: fileNameWithExtension,
