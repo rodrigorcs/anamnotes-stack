7b24e8e8c0529ef1f11210d59b69c3546dbee69e
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2024-07-10 08:37:52 -0300

    Added cognito

---
+++ infra/lib/constructs/cognito/client.ts
@@ -0,0 +1,46 @@
+import { aws_cognito as cognito, Duration } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+
+interface IUserPoolClientProps extends cognito.UserPoolClientProps {}
+
+export class UserPoolClient {
+  public readonly userPoolClient: cognito.UserPoolClient
+
+  constructor(scope: Construct, props: IUserPoolClientProps) {
+    const userPoolClientName = `${props.userPool.userPoolId}-client`
+    const standardCognitoAttributes: cognito.StandardAttributesMask = {
+      fullname: true,
+      email: true,
+      emailVerified: true,
+    }
+
+    // Granting permissions for readable fields on cognito
+    const clientReadAttributes = new cognito.ClientAttributes()
+      .withStandardAttributes(standardCognitoAttributes)
+      .withCustomAttributes(...['isAdmin'])
+
+    // Granting permissions for writeable fields on cognito
+    const clientWriteAttributes = new cognito.ClientAttributes().withStandardAttributes({
+      ...standardCognitoAttributes,
+      emailVerified: false,
+    })
+
+    this.userPoolClient = new cognito.UserPoolClient(scope, userPoolClientName, {
+      authFlows: {
+        userSrp: true,
+      },
+      supportedIdentityProviders: [
+        cognito.UserPoolClientIdentityProvider.COGNITO,
+        cognito.UserPoolClientIdentityProvider.GOOGLE,
+        cognito.UserPoolClientIdentityProvider.FACEBOOK,
+        cognito.UserPoolClientIdentityProvider.APPLE,
+      ],
+      readAttributes: clientReadAttributes,
+      writeAttributes: clientWriteAttributes,
+      accessTokenValidity: Duration.minutes(60),
+      idTokenValidity: Duration.minutes(60),
+      refreshTokenValidity: Duration.days(90),
+      ...props,
+    })
+  }
+}
---
+++ infra/lib/constructs/cognito/index.ts
@@ -0,0 +1,74 @@
+import { config } from '../../../config'
+import { aws_cognito as cognito, RemovalPolicy } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import { LambdaFunction } from '../lambda'
+
+interface IUserPoolProps {
+  domainPrefix: string
+  name?: string
+  migrationLambda?: LambdaFunction
+}
+export class UserPool {
+  public readonly userPool: cognito.UserPool
+
+  constructor(scope: Construct, props: IUserPoolProps) {
+    const userPoolName = `${config.projectName}${props.name ? `-${props.name}` : ''}-user-pool`
+
+    this.userPool = new cognito.UserPool(scope, userPoolName, {
+      userPoolName,
+      selfSignUpEnabled: true,
+      signInAliases: {
+        email: true,
+      },
+      autoVerify: {
+        email: true,
+      },
+      standardAttributes: {
+        fullname: {
+          required: true,
+          mutable: true,
+        },
+      },
+      customAttributes: {
+        isAdmin: new cognito.StringAttribute({ mutable: true }),
+      },
+      passwordPolicy: {
+        minLength: 8,
+        requireLowercase: true,
+        requireDigits: true,
+        requireUppercase: true,
+        requireSymbols: true,
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      removalPolicy: RemovalPolicy.RETAIN,
+      lambdaTriggers: {},
+      userInvitation: {
+        emailSubject: 'Anamnotes - Código de verificação',
+        emailBody: 'Bem vindo ao Anamnotes! O seu código de verificação é "{####}".',
+      },
+      userVerification: {
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+        emailSubject: 'Anamnotes - Código de verificação',
+        emailBody: 'O seu código de verificação do Anamnotes é "{####}".',
+      },
+    })
+
+    const cfnUserPool = this.userPool.node.defaultChild as cognito.CfnUserPool
+    cfnUserPool.emailConfiguration = {
+      emailSendingAccount: 'COGNITO_DEFAULT',
+    }
+
+    if (props.migrationLambda) {
+      this.userPool.addTrigger(
+        cognito.UserPoolOperation.USER_MIGRATION,
+        props.migrationLambda.lambdaFn,
+      )
+    }
+
+    this.userPool.addDomain(`${userPoolName}-domain`, {
+      cognitoDomain: {
+        domainPrefix: props.domainPrefix,
+      },
+    })
+  }
+}
